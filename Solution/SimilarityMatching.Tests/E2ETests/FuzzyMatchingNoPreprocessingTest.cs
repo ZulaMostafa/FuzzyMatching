292050f0c3c23d958df8e715bb73b213f39eff9a
MIDDLEEAST\v-moshaban <v-moshaban@microsoft.com>
2021-02-17 18:56:59 +0200

    [tests] add e2e test for naive algorithm

---
+++ Solution/SimilarityMatching.Tests/E2ETests/FuzzyMatchingNoPreprocessingTest.cs
@@ -0,0 +1,30 @@
+﻿using FuzzyMatching.Algorithms;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FuzztMatching.Tests.E2ETests
+{
+    public class FuzzyMatchingNoPreprocessingTest
+    {
+        [Fact]
+        public void TestAlgorithm()
+        {
+            // get test data
+            string sentence = "lets play some games";
+            var sentenceDataset = new List<string>
+            {
+                "go out",
+                "drink water",
+                "do laundry",
+                "play games",
+                "blah blah blah"
+            };
+
+            // calculate similarity (no preprocessing)
+            var result = FuzzyMatchingNoPreprocessing.GetClosestSentence(sentence, sentenceDataset);
+            var closestMatch = result.Item1;
+            var index = result.Item2;
+            var score = result.Item3;
+        }
+    }
+}
