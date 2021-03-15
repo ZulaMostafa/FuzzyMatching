97d9712a4654641bdb915b22b7c09fcce12dde89
karimelhammady <31138502+karimelhammady@users.noreply.github.com>
2021-03-15 23:09:57 +0200

    adding console prints

--- Solution/SimilarityMatching/Algorithms/FuzzyMatchingNoPreprocessing.cs
+++ Solution/SimilarityMatching/Algorithms/FuzzyMatchingNoPreprocessing.cs
@@ -1,5 +1,6 @@
 ﻿using FuzztMatching.Core.FeatureMatrixCalculation;
 using FuzztMatching.Core.MatrixOperations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -21,31 +22,55 @@ namespace FuzzyMatching.Algorithms
             // calculate ngrams for each sentence
             var ngramsLength = 3;
             var inputSentenceNGrams = NGramsCalculator.GetSentenceNGramsAsync(sentence, ngramsLength).GetAwaiter().GetResult();
+            DateTime start = DateTime.Now;
             var inputSentenceDatasetNGrams = NGramsCalculator.GetSentenceNGramsBatchAsync(sentenceDataset, ngramsLength).GetAwaiter().GetResult();
+            DateTime end = DateTime.Now;
+            TimeSpan ts = (end - start);
+            Console.WriteLine("Elapsed Time for the Ngrms calc is {0} ms", ts.Milliseconds);
 
             // calculate ngram frequencies
             var inputSentenceNGramFrequencies = FrequencyCalculator.GetNGramFrequencyAsync(inputSentenceNGrams).GetAwaiter().GetResult();
+
+            start = DateTime.Now;
             var inputSentenceDatasetNGramFrequencies = FrequencyCalculator.GetNGramFrequencyBatchAsync(inputSentenceDatasetNGrams).GetAwaiter().GetResult();
             var allSenteceList = inputSentenceDatasetNGrams.Append(inputSentenceNGrams).ToArray();
             var overallDataNgramFrequencies = FrequencyCalculator.GetOverallNGramFrequencyAsync(allSenteceList).GetAwaiter().GetResult();
+            end = DateTime.Now;
+            ts = (end - start);
+            Console.WriteLine("Elapsed Time for the ngram frequencies is {0} ms", ts.Milliseconds);
 
             // get ngrams feature vector
             var allDataUniqueNGramsVector = overallDataNgramFrequencies.Keys.ToArray();
 
             // calculate TF
             var inputSentenceTFVector = TFCalculator.CalculateTFVectorAsync(inputSentenceNGramFrequencies, allDataUniqueNGramsVector).GetAwaiter().GetResult();
+            start = DateTime.Now;
             var inputSentenceDatasetTFMatrix = TFCalculator.CalculateTFVectorBatchAsync(inputSentenceDatasetNGramFrequencies, allDataUniqueNGramsVector).GetAwaiter().GetResult();
-
+            end = DateTime.Now;
+            ts = (end - start);
+            Console.WriteLine("Elapsed Time for the TF calc is {0} ms", ts.TotalMilliseconds);
             // calculate IDF
             int overallDataLength = sentenceDataset.Count + 1;
+            start = DateTime.Now;
             var overallDataIDFVector = IDFCalculator.CalculateIDFVector(allDataUniqueNGramsVector, overallDataNgramFrequencies, overallDataLength);
+            end = DateTime.Now;
+            ts = (end - start);
+            Console.WriteLine("Elapsed Time for the IDF calc is {0} ms", ts.TotalMilliseconds);
 
             // calculate TF-IDF
             var inputSentenceTFIDFVector = CellOperations.MultiplyVectorCells(inputSentenceTFVector, overallDataIDFVector);
+            start = DateTime.Now;
             var inputSentenceDatasetTFIDFMatrix = CellOperations.MultiplyVectorCellsBatch(inputSentenceDatasetTFMatrix, overallDataIDFVector);
+            end = DateTime.Now;
+            ts = (end - start);
+            Console.WriteLine("Elapsed Time for the TF-IDF calc is {0} ms", ts.TotalMilliseconds);
 
             // get dot product
+            start = DateTime.Now;
             var similarityValues = DotProductCalculator.CalculateDotProduct(inputSentenceTFIDFVector, inputSentenceDatasetTFIDFMatrix);
+            end = DateTime.Now;
+            ts = (end - start);
+            Console.WriteLine("Elapsed Time for the dot product is {0} ms", ts.TotalMilliseconds);
 
             // get most matching one
             float minValue = similarityValues.Min();
